239127424ae2939d3480273529443d0ac33b4e13
IsuruRaveen20 <isururaveen4520@gmail.com>
2025-03-24 23:30:29 +0530

    DK-60: Gulp Build issue fixed - Fixed deprecation warning

--- web/themes/custom/themag/gulpfile.js
+++ web/themes/custom/themag/gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, task, series, watch } = require("gulp");
 
-// Import Gulp plugins.
+// Import Gulp plugins
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
@@ -11,45 +11,97 @@ const run = require('gulp-run');
 
 // Transpile JavaScripts
 const javascript = () => {
-  return src('./_src/themag/js/**/*.js')
-    // Init source maps
-    // .pipe(sourcemaps.init())
-    // Stop the process if an error is thrown.
-    .pipe(plumber())
-    // Transpile the JS code using Babel's preset-env.
-    .pipe(babel({
-      presets: [
-        ['@babel/env', {
-          modules: false
-        }]
-      ]
-    }))
-    // .pipe(sourcemaps.write())
-    // Save each component as a separate file in dist.
-    .pipe(uglify())
-    .pipe(dest('./assets/js'))
+    return src(['./_src/themag/js/**/*.js', '!./_src/themag/js/**/*.min.js'])
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(babel({
+            presets: [
+                ['@babel/env', {
+                    modules: false
+                }]
+            ]
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./assets/js'));
 };
-
 
 // Compile SCSS
 const scss = () => {
-  sass.compiler = require('sass');
-  return src('./_src/themag/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded',
-      includePaths: ['./node_modules']
-    }).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'ie 11'))
-    .pipe(sourcemaps.write('./sourcemap/'))
-    .pipe(dest('./assets/css'));
+    return src('./_src/themag/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(plumber()) // Add plumber to prevent watch crashes
+        .pipe(sass({
+            outputStyle: 'expanded',
+            includePaths: ['./node_modules']
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions', 'ie 11'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./assets/css'));
 };
 
 // Define tasks
 task('scss', scss);
 task('javascript', javascript);
 
+// Default task (Watch for changes)
 exports.default = () => {
-  watch(['./_src/themag/scss/**/*.scss'], scss);
-  watch(['./_src/themag/js/**/*.js'], javascript);
+    watch(['./_src/themag/scss/**/*.scss'], scss);
+    watch(['./_src/themag/js/**/*.js'], javascript);
 };
+// const { src, dest, task, series, watch } = require("gulp");
+//
+// // Import Gulp plugins.
+// const sass = require('gulp-sass')(require('sass'));
+// const autoprefixer = require('gulp-autoprefixer');
+// const sourcemaps = require('gulp-sourcemaps');
+// const babel = require('gulp-babel');
+// const plumber = require('gulp-plumber');
+// const uglify = require('gulp-uglify');
+// const run = require('gulp-run');
+//
+// // Transpile JavaScripts
+// const javascript = () => {
+//     return src(['./_src/themag/js/**/*.js', '!./_src/themag/js/**/*.min.js']) // Ignore min.js files
+//         .pipe(sourcemaps.init())
+//         .pipe(plumber())
+//         .pipe(babel({
+//             presets: [
+//                 ['@babel/env', {
+//                     modules: false
+//                 }]
+//             ]
+//         }))
+//         .pipe(uglify())
+//         .pipe(sourcemaps.write('.')) // Inline source maps
+//         .pipe(dest('./assets/js'));
+// };
+//
+// // Compile SCSS
+// const scss = () => {
+//     return src('./_src/themag/scss/**/*.scss')
+//         .pipe(sourcemaps.init())
+//         .pipe(sass({
+//             outputStyle: 'expanded',
+//             includePaths: ['./node_modules']
+//         }).on('error', sass.logError))
+//         .pipe(autoprefixer({
+//             overrideBrowserslist: ['last 2 versions', 'ie 11'],
+//             cascade: false
+//         }))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(dest('./assets/css'));
+// };
+//
+// // Define tasks
+// task('scss', scss);
+// task('javascript', javascript);
+//
+// // Default task (Watch for changes)
+// exports.default = () => {
+//     watch(['./_src/themag/scss/**/*.scss'], scss);
+//     watch(['./_src/themag/js/**/*.js'], javascript);
+// };
